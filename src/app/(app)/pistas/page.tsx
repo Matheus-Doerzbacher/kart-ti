4fc0d1012de1eaf087c5ced56ad8a1a841516599
Matheus-NUC <matheus.doer@gmail.com>
2024-08-01 17:09:24 -0300

    Ajustada a pagina que apressenta as pistas

--- src/app/(app)/pistas/page.tsx
+++ src/app/(app)/pistas/page.tsx
@@ -1,7 +1,19 @@
-import { pistasMock } from '@/mock/pistasMock'
+'use client'
 import { CardPista } from './_components/card_pista'
+import { getAllPista, Pista } from '@/services/pista'
+import { useEffect, useState } from 'react'
 
 export default function Page() {
+  const [pistas, setPistas] = useState<Pista[]>([])
+
+  useEffect(() => {
+    const buscarPistas = async () => {
+      const data = await getAllPista()
+      setPistas(data)
+    }
+    buscarPistas()
+  }, [])
+
   return (
     <div className="w-full max-w-6xl mx-auto px-4 py-12 md:py-16 lg:py-20">
       <div className="space-y-6 text-center">
@@ -10,7 +22,7 @@ export default function Page() {
         </h1>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
-        {pistasMock.map((pista) => (
+        {pistas.map((pista) => (
           <CardPista key={pista.id} pista={pista} />
         ))}
       </div>
